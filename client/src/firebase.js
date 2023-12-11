05be87f22ec9b24819dc3049719879be858e1315
kshitijmeshram30 <kshitij.meshram.3097@gmail.com>
2023-12-11 21:09:43 +0530

    Fixed SignUp with Google

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: "mern-freedom-14789.firebaseapp.com",
+  projectId: "mern-freedom-14789",
+  storageBucket: "mern-freedom-14789.appspot.com",
+  messagingSenderId: "834419962210",
+  appId: "1:834419962210:web:9e4d00d36e4a1e89517c3b"
 };
 
 // Initialize Firebase
